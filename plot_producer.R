f518a1e6b605d7d63f55ee0ee1ea016abedbc3f4
abrahammbs93 <a.abraham@mbsconsulting.it>
2024-03-28 15:04:27 +0100

    plot producer phase 1

--- plot_producer.R
+++ plot_producer.R
@@ -1,74 +1,53 @@
 
+
 # Packages and Setup ------------------------------------------------------------------
 
-library(mbsenergyUtils)
+# library(mbsenergyUtils)
 library(ggplot2)
 library(data.table)
-library(openxlsx)
-library(flextable)
+# library(openxlsx)
+# library(flextable)
+# 
+# set_flextable_defaults(
+#     font.color = "#747E7E",
+#     font.size = 9,
+#     font.family = 'calibri',
+#     padding = 5,
+#     border.color = "#dfe2e5",
+#     background.color = "whitesmoke",
+#     split = FALSE,
+#     theme_fun = "theme_box",
+#     decimal.mark = ",",
+#     big.mark = " ",
+#     na_str = "<na>")
 
-set_flextable_defaults(
-    font.color = "#747E7E",
-    font.size = 9,
-    font.family = 'calibri',
-    padding = 5,
-    border.color = "#dfe2e5",
-    background.color = "whitesmoke",
-    split = FALSE,
-    theme_fun = "theme_box",
-    decimal.mark = ",",
-    big.mark = " ",
-    na_str = "<na>")
 
-# Upload File ---------------------------------------
 
-## Tables -------------------------
+## GDP Growth Rate -------------------------
 
-dt_tables = openxlsx::read.xlsx(file.path('report_gen', 'data', 'tables_report.xlsx'), sheet = 'Sheet0') %>% 
-    setDT() 
+dt_gdp_gwr = openxlsx::read.xlsx(file.path('data', 'Grafici report scenari.xlsx'), sheet = 1) %>% setDT() 
 
-### Table 1
+dt_gdp_gwr_lg = dt_gdp_gwr %>% 
+    melt(id.vars = 'GDP.Growth.Rate.(%)', variable.name = 'anni', value.name = 'valori') 
 
-dt_1 =
-  melt(dt_tables, id.vars = c(1,2), variable.name = 'Year', value.name = 'Values') %>% 
-    dcast(NET.POWER ~ Scenario + Year, value.var = 'Values')
-setcolorder(dt_1, neworder = c('NET.POWER', 'Reference_2025', 'Reference_2030', 'Reference_2040', 'Reference_2050',
-                                           'Low_2025', 'Low_2030', 'Low_2040', 'Low_2050',
-                                           'High_2025', 'High_2030', 'High_2040', 'High_2050'))
-
-dt_1 |> flextable() %>% 
-    separate_header() %>%  
-    align(align = "center", part = "all") %>% 
-    bg(bg = "#B2BEBF", part = "header") %>% 
-    color(part = "header", color = 'white') %>% 
-    vline(j = c('NET.POWER', 'Reference_2050', 'Low_2050', 'High_2050'), border = officer::fp_border(color = "#dfe2e5", width = 2), part = "all") %>% 
-    bold(i = NULL, j = 1) %>% 
-    width(width = 2, j = 1) 
+dt_gdp_gwr_lg[, anni := as.numeric(anni)]
+setorderv(dt_gdp_gwr_lg, cols = c('GDP.Growth.Rate.(%)', 'anni'))
 
         
-
-
-## Plots -----------------------
-dt_plots_6 = openxlsx::read.xlsx(file.path('report_gen', 'data', 'dati_grafici_report_scenario.xlsx'), sheet = '6.TTF') %>% 
-    setDT() %>% 
-        melt(id.vars = 'Scenario', variable.name = 'Year', value.name = 'Values') %>% 
-            .[, Year := as.numeric(as.character(Year))] %>% 
-            setorder(Year, Scenario)  
-
-
-plot_6 =
-    dt_plots_6 %>%
-    ggplot(aes(x = Year,
-               y = Values,
-               color = Scenario)) +
+plot_1 =
+    dt_gdp_gwr_lg %>%
+    ggplot(aes(x = anni,
+               y = valori,
+               color = `GDP.Growth.Rate.(%)`)) +
     geom_point() +
-    geom_line() +
+    geom_line(group = 1) +
         theme_light() +
             theme(legend.position = 'bottom') +
             labs(x = NULL,
-                 y = '€/MWh')
+                 y = '€/MWh',
+                 caption = 'Elaborazione MBS')
 
-plot_6
+plot_1
 
 ggsave(file.path('report_gen', 'figs', '6_ttf.png'), plot_6,
        width = 8, height = 3)
