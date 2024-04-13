f5d1204dd908c9ef65dba4bdb2bdd21337aacba6
abrahammbs93 <a.abraham@mbsconsulting.it>
2024-04-13 09:27:01 +0200

    new report style with mbsenergyUtils 1.0.2

---
+++ table_producer.R
@@ -0,0 +1,14 @@
+library(flextable)
+
+set_flextable_defaults(
+    font.color = "#747E7E",
+    font.size = 9,
+    font.family = 'calibri',
+    padding = 5,
+    border.color = "#dfe2e5",
+    background.color = "whitesmoke",
+    split = FALSE,
+    theme_fun = "theme_box",
+    decimal.mark = ",",
+    big.mark = " ",
+    na_str = "<na>")
